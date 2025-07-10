71710e943ccd487ac464d1b009cb022e4d41d8d8
Trust-Worthy <jondeveloper0@gmail.com>
2025-07-10 09:20:55 -0600

    Importing libraries.

---
+++ qr_crypt.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+### used to generate keysand sign/ verify messages
+from cryptography.hazmat.primitives.asymmetric import ed25519 
+
+### used to convert keys (like pub key) into byte formats so they can be saved, sent, or shared
+from cryptography.hazmat.primitives import serialization
+
+###

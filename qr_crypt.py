ca697a20d31795c4cb3c059d62b25a8d556a305a
Trust-Worthy <jondeveloper0@gmail.com>
2025-07-10 16:07:49 -0600

    Going to check the qr code shows correctly.

--- qr_crypt.py
+++ qr_crypt.py
@@ -23,16 +23,44 @@ import time
 
 
 # --- Step 1: Generate Key pair (this is Alice)
-private_key = ed25519.Ed25519PrivateKey.generate()
-public_key = private_key.public_key()
+alice_private_key = ed25519.Ed25519PrivateKey.generate()
+alice_public_key = alice_private_key.public_key()
 
 # --- Step 2: Save public key in a sharable form (e.g. QR)
-pub_bytes = public_key.public_bytes(
+pub_bytes = alice_public_key.public_bytes(
     encoding=serialization.Encoding.Raw,
     format=serialization.PublicFormat.Raw
 )
 
-# --- S
+# --- Step 3: encodes the bytes to make them readable and embedabble in JSON or QR
+pub_b64 = base64.b64encode(pub_bytes).decode()
+
+# --- Generate a challenge
+nonce = secrets.token_hex(16) # one time use token
+timestamp = int(time.time()) # gets current time
+challenge = f"verify:{nonce}:{timestamp}" # creates challenge string that will be signed.
+print(f"Challenge: {challenge}")
+
+# --- Step 4: Alice sings the challenge
+signed = alice_private_key.sign(challenge.encode)
+signature_b64 = base64.b64encode(signed).decode()
+# --- Step 5: Package response as a QR
+
+payload = {
+    "public_key": pub_b64, ### pub key so others can verify
+    "challenge": challenge, ### original challenge so people know what was signed
+    "signature": signature_b64 ### signature
+}
+
+payload_json = json.dumps(payload)
+
+### Step 6: Create QR code
+qr = qrcode.make(payload_json)
+qr.show()
+
+
+
+###
 
 ### TO-DO ###
 '''
